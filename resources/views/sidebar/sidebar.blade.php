55d70e73f506c9f7e304a7dda40ea69f7fac833e
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2023-07-22 19:09:04 +0100

    adjusting the sidebar for diff role types

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -174,40 +174,24 @@
                         <li class="menu-title">
                             <span>Main Menu</span>
                         </li>
-                        <li class="{{ set_active(['setting/page']) }}">
-                            <a href="{{ route('setting/page') }}">
-                                <i class="fas fa-cog"></i>
-                                <span>Settings</span>
+                        <li class="{{ set_active(['home']) }}">
+                            <a href="{{ route('home') }}" class="{{ set_active(['home']) }}">
+                                <i class="feather-grid"></i><span> Dashboard</span>
                             </a>
                         </li>
-                        <li class="submenu {{ set_active(['home', 'teacher/dashboard', 'student/dashboard']) }}">
-                            <a href="#"><i class="feather-grid"></i>
-                                <span> Dashboard</span>
+
+                        <li
+                            class="submenu {{ set_active(['list/users']) }} {{ request()->is('view/user/edit/*') ? 'active' : '' }}">
+                            <a href="#"><i class="fas fa-shield-alt"></i>
+                                <span>User Management</span>
                                 <span class="menu-arrow"></span>
                             </a>
                             <ul>
-                                <li><a href="{{ route('home') }}" class="{{ set_active(['home']) }}">Admin
-                                        Dashboard</a></li>
-                                <li><a href="{{ route('teacher/dashboard') }}"
-                                        class="{{ set_active(['teacher/dashboard']) }}">Teacher Dashboard</a></li>
-                                <li><a href="{{ route('student/dashboard') }}"
-                                        class="{{ set_active(['student/dashboard']) }}">Student Dashboard</a></li>
+                                <li><a href="{{ route('list/users') }}"
+                                        class="{{ set_active(['list/users']) }} {{ request()->is('view/user/edit/*') ? 'active' : '' }}">List
+                                        Users</a></li>
                             </ul>
                         </li>
-                        @if (Session::get('role_name') === 'Admin' || Session::get('role_name') === 'Super Admin')
-                            <li
-                                class="submenu {{ set_active(['list/users']) }} {{ request()->is('view/user/edit/*') ? 'active' : '' }}">
-                                <a href="#"><i class="fas fa-shield-alt"></i>
-                                    <span>User Management</span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul>
-                                    <li><a href="{{ route('list/users') }}"
-                                            class="{{ set_active(['list/users']) }} {{ request()->is('view/user/edit/*') ? 'active' : '' }}">List
-                                            Users</a></li>
-                                </ul>
-                            </li>
-                        @endif
 
                         <li
                             class="submenu {{ set_active(['student/list', 'student/grid', 'student/add/page']) }} {{ request()->is('student/edit/*') ? 'active' : '' }} {{ request()->is('student/profile/*') ? 'active' : '' }}">
@@ -335,105 +319,12 @@
                         <li class="menu-title">
                             <span>Main Menu</span>
                         </li>
-                        <li class="{{ set_active(['setting/page']) }}">
-                            <a href="{{ route('setting/page') }}">
-                                <i class="fas fa-cog"></i>
-                                <span>Settings</span>
+                        <li class="{{ set_active(['home']) }}">
+                            <a href="{{ route('home') }}" class="{{ set_active(['home']) }}">
+                                <i class="feather-grid"></i><span> Dashboard</span>
                             </a>
-                        </li>
-                        <li class="submenu {{ set_active(['home', 'teacher/dashboard', 'student/dashboard']) }}">
-                            <a href="#"><i class="feather-grid"></i>
-                                <span> Dashboard</span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <ul>
-                                <li><a href="{{ route('home') }}" class="{{ set_active(['home']) }}">Admin
-                                        Dashboard</a></li>
-                                <li><a href="{{ route('teacher/dashboard') }}"
-                                        class="{{ set_active(['teacher/dashboard']) }}">Teacher Dashboard</a></li>
-                                <li><a href="{{ route('student/dashboard') }}"
-                                        class="{{ set_active(['student/dashboard']) }}">Student Dashboard</a></li>
-                            </ul>
-                        </li>
-                        @if (Session::get('role_name') === 'Admin' || Session::get('role_name') === 'Super Admin')
-                            <li
-                                class="submenu {{ set_active(['list/users']) }} {{ request()->is('view/user/edit/*') ? 'active' : '' }}">
-                                <a href="#"><i class="fas fa-shield-alt"></i>
-                                    <span>User Management</span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul>
-                                    <li><a href="{{ route('list/users') }}"
-                                            class="{{ set_active(['list/users']) }} {{ request()->is('view/user/edit/*') ? 'active' : '' }}">List
-                                            Users</a></li>
-                                </ul>
-                            </li>
-                        @endif
-
-                        <li
-                            class="submenu {{ set_active(['student/list', 'student/grid', 'student/add/page']) }} {{ request()->is('student/edit/*') ? 'active' : '' }} {{ request()->is('student/profile/*') ? 'active' : '' }}">
-                            <a href="#"><i class="fas fa-graduation-cap"></i>
-                                <span> Students</span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <ul>
-                                <li><a href="{{ route('student/list') }}"
-                                        class="{{ set_active(['student/list', 'student/grid']) }}">Student List</a>
-                                </li>
-                                <li><a href="{{ route('student/add/page') }}"
-                                        class="{{ set_active(['student/add/page']) }}">Student Add</a></li>
-                                <li><a class="{{ request()->is('student/edit/*') ? 'active' : '' }}">Student Edit</a>
-                                </li>
-                                <li><a href=""
-                                        class="{{ request()->is('student/profile/*') ? 'active' : '' }}">Student
-                                        View</a>
-                                </li>
-                            </ul>
                         </li>
 
-                        <li
-                            class="submenu  {{ set_active(['teacher/add/page', 'teacher/list/page', 'teacher/grid/page', 'teacher/edit']) }} {{ request()->is('teacher/edit/*') ? 'active' : '' }}">
-                            <a href="#"><i class="fas fa-chalkboard-teacher"></i>
-                                <span> Teachers</span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <ul>
-                                <li><a href="{{ route('teacher/list/page') }}"
-                                        class="{{ set_active(['teacher/list/page', 'teacher/grid/page']) }}">Teacher
-                                        List</a>
-                                </li>
-                                <li><a href="teacher-details.html">Teacher View</a></li>
-                                <li><a href="{{ route('teacher/add/page') }}"
-                                        class="{{ set_active(['teacher/add/page']) }}">Teacher Add</a></li>
-                                <li><a class="{{ request()->is('teacher/edit/*') ? 'active' : '' }}">Teacher Edit</a>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li class="submenu {{ set_active(['department/add/page', 'department/edit/page']) }}">
-                            <a href="#"><i class="fas fa-building"></i>
-                                <span> Departments</span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <ul>
-                                <li><a href="departments.html">Department List</a></li>
-                                <li><a href="{{ route('department/add/page') }}"
-                                        class="{{ set_active(['department/add/page']) }}">Department Add</a></li>
-                                <li><a href="{{ route('department/edit/page') }}"
-                                        class="{{ set_active(['department/edit/page']) }}">Department Edit</a></li>
-                            </ul>
-                        </li>
-                        <li class="submenu">
-                            <a href="#"><i class="fas fa-book-reader"></i>
-                                <span> Subjects</span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <ul>
-                                <li><a href="subjects.html">Subject List</a></li>
-                                <li><a href="add-subject.html">Subject Add</a></li>
-                                <li><a href="edit-subject.html">Subject Edit</a></li>
-                            </ul>
-                        </li>
                         <li class="submenu">
                             <a href="#"><i class="fas fa-clipboard"></i>
                                 <span> Invoices</span>
@@ -448,9 +339,7 @@
                                 <li><a href="invoices-settings.html">Invoices Settings</a></li>
                             </ul>
                         </li>
-                        <li class="menu-title">
-                            <span>Management</span>
-                        </li>
+
                         <li class="submenu">
                             <a href="#"><i class="fas fa-file-invoice-dollar"></i>
                                 <span> Accounts</span>
@@ -472,16 +361,7 @@
                             <a href="fees.html"><i class="fas fa-comment-dollar"></i> <span>Fees</span></a>
                         </li>
                         <li>
-                            <a href="exam.html"><i class="fas fa-clipboard-list"></i> <span>Exam list</span></a>
-                        </li>
-                        <li>
                             <a href="event.html"><i class="fas fa-calendar-day"></i> <span>Events</span></a>
-                        </li>
-                        <li>
-                            <a href="time-table.html"><i class="fas fa-table"></i> <span>Time Table</span></a>
-                        </li>
-                        <li>
-                            <a href="library.html"><i class="fas fa-book"></i> <span>Library</span></a>
                         </li>
                     </ul>
                 </div>
@@ -496,9 +376,10 @@
                         <li class="menu-title">
                             <span>Main Menu</span>
                         </li>
-                        <li>
-                            <a href="{{ route('student/dashboard') }}"class="{{ set_active(['student/dashboard']) }}">
-                            <i class="feather-grid"></i><span>Student Dashboard</span></a>
+                        <li class="{{ set_active(['student/dashboard']) }}">
+                            <a
+                                href="{{ route('student/dashboard') }}"class="{{ set_active(['student/dashboard']) }}">
+                                <i class="feather-grid"></i><span>Student Dashboard</span></a>
                         </li>
 
                         <li class="submenu">
@@ -509,10 +390,9 @@
                             <ul>
                                 <li><a href="subjects.html">Subject List</a></li>
                                 <li><a href="add-subject.html">Subject Add</a></li>
-                                <li><a href="edit-subject.html">Subject Edit</a></li>
                             </ul>
                         </li>
-                        
+
                         <li>
                             <a href="holiday.html"><i class="fas fa-holly-berry"></i> <span>Holiday</span></a>
                         </li>
@@ -544,9 +424,10 @@
                         <li class="menu-title">
                             <span>Main Menu</span>
                         </li>
-                        <li>
+                        <li class="{{ set_active(['teacher/dashboard']) }}">
 
-                            <a href="{{ route('teacher/dashboard') }}"class="{{ set_active(['teacher/dashboard']) }}">
+                            <a
+                                href="{{ route('teacher/dashboard') }}"class="{{ set_active(['teacher/dashboard']) }}">
                                 <i class="feather-grid"></i><span>Teacher Dashboard</span>
                             </a>
                         </li>
@@ -587,9 +468,6 @@
                             <a href="holiday.html"><i class="fas fa-holly-berry"></i> <span>Holiday</span></a>
                         </li>
                         <li>
-                            <a href="fees.html"><i class="fas fa-comment-dollar"></i> <span>Fees</span></a>
-                        </li>
-                        <li>
                             <a href="exam.html"><i class="fas fa-clipboard-list"></i> <span>Exam list</span></a>
                         </li>
                         <li>
@@ -606,5 +484,5 @@
             </div>
         </div>
     @endif
-    
+
 @endif
